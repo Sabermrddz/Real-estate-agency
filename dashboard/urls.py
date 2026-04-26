4dca1b60a17720b46e9acdd5d9b619c922738e9a
saber <sabermrddz@gmail.com>
2026-04-26 20:29:28 +0100

    REP-72 i have fixed urls file the problem was bad defining pth naming in the delete inquiry route

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('', views.DashboardOverviewView.as_view(), name='overview'),
     path('listings/', views.MyListingsView.as_view(), name='listings'),
     path('inbox/', views.MyInboxView.as_view(), name='inbox'),
-    path('inbdox/<int:inquiry_id>/delete/', views.delete_inquiry, name='delete_inquiry'),
+    path('inbox/<int:inquiry_id>/delete/', views.delete_inquiry, name='delete_inquiry'),
     path('reservations/', views.MyReservationsView.as_view(), name='reservations'),
     path('reservations/<int:reservation_id>/<str:action>/', views.reservation_action, name='reservation_action'),
 ]
